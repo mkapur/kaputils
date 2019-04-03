94d391cc8eeeb9930fa14557f78efad97287d956
mkapur <kapur.mr@gmail.com>
2019-04-03 16:24:26 -0700

    add compareNA
    http://www.cookbook-r.com/Manipulating_data/Comparing_vectors_or_factors_with_NA/

---
+++ R/compareNA.R
@@ -0,0 +1,12 @@
+#' compareNA
+#'
+#' \code{compareNA} taken from R cookbook -- enables comparison of two things (strings/numbers/whatever) that may or may not contain NA. Will return TRUE if both  are equal, including if both are NA.
+#' @param v1 first thing, can be NA
+#' @param v2 second thing, can be NA
+#' @seealso \link[ http://www.cookbook-r.com/Manipulating_data/Comparing_vectors_or_factors_with_NA/]
+
+compareNA <- function(v1,v2) {
+  same <- (v1 == v2) | (is.na(v1) & is.na(v2))
+  same[is.na(same)] <- FALSE
+  return(same)
+}
