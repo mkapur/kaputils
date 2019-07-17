6e9627e154d940a51dbdbe876beeabaf007d49b5
Maia Sosa Kapur <kapurm@uw.edu>
2019-07-17 13:14:50 -0700

    Update compareNA.R

--- R/compareNA.R
+++ R/compareNA.R
@@ -1,5 +1,4 @@
 #' compareNA
-#' taken from R cookbook -- enables comparison of two things (strings/numbers/whatever) that may or may not contain NA. Will return TRUE if both  are equal, including if both are NA.
 #' @param v1 first thing, can be NA
 #' @param v2 second thing, can be NA
 #' @seealso \link[ http://www.cookbook-r.com/Manipulating_data/Comparing_vectors_or_factors_with_NA/]
